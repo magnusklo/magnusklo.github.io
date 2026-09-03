e93e10db677f8cd25528bb034f71e011caeb38f8
magnusklo <magnusklo@gmail.com>
2026-09-03 19:03:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='skemaur-github-v6-dagskift-fix';
+const CACHE='skemaur-github-v7-editor-first-click-fix';
 const SHELL=['./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png','./icons/apple-touch-icon.png',
   './ding.mp3'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting()});
